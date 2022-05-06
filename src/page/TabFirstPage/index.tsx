0fff05343cc1f8b3a2d43967634e38f9b92af2fa
Chipmunk-Woori <haha51015@naver.com>
2022-05-06 18:52:04 +0900

    BottomTab

--- src/page/TabFirstPage/index.tsx
+++ src/page/TabFirstPage/index.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import {View} from "react-native";
+import TabFirstContainer from "../../container/TabFirstContainer";
+
+interface Props {
+
+}
 
 const TabFirstPage = () => {
-
+    return(
+        <>
+            {/* <TabFirstContainer/> */}
+        </>
+    )
 }
 
 export default TabFirstPage;
